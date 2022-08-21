1fa1c717af736fa0953ea09c492918d6a5caccd0
Zain Ul Abideen <zainjaveed97@gmail.com>
2022-08-21 17:12:44 +0500

    initial setup

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+const express = require('express');
+const app = express();
+const ConnectDatabase = require('./db/connect');
+require('dotenv').config();
+const NotFound = require('./middlewares/not-found');
+
+
+app.use(express.json());
+
+// app.use('/api/v1/tasks', TaskRoutes);
+app.use(NotFound);
+
+
+app.get('/', (req, res) => {
+    res.end('Node Store Api!');
+});
+
+
+const port = process.env.PORT || 3000;
+
+
+async function RunServer() {
+    try {
+        await ConnectDatabase(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+RunServer();
